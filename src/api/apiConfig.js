8a1e29f9d533e9a76180d8466356262b9b217d0a
SDInHearts <63344741+SDInHearts@users.noreply.github.com>
2025-02-23 22:22:49 +0600

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,5 +1,6 @@
 const apiConfig = {
-  baseUrl: "https://api.themoviedb.org/3/",
+  // baseUrl: "https://api.themoviedb.org/3/",
+  baseUrl: "https://aescraper.netlify.app/",
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 }
